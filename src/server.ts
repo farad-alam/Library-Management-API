cbdabdb0e43e714397bd17463ae1fe396e5f8af6
Farad Alam <farad.dev@gmail.com>
2025-06-20 22:29:16 +0600

    db connected

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,30 @@
+import mongoose from "mongoose";
 import app from "./app";
+import {Server} from 'http'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const PORT = 3000 
+const PORT = 3000;
+
+let server:Server
+
+let mongoDB_uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@blogcluster.tzdrh.mongodb.net/l2_lms?retryWrites=true&w=majority&appName=BlogCluster`;
 
 async function main() {
-    try {
-        app.listen(PORT, () => {
-          console.log(`App is listingin on port ${PORT}`);
-        });
-    } catch (error) {
-        
-         const resObj = {
-          message: "Server listning failed",
-          success: false,
-          error : error,
-        };
-        console.log(`Here is the server error`,resObj);
-
-        
-    }
+  try {
+    await mongoose.connect(mongoDB_uri)
+    console.log("DB Connected");
+    server = app.listen(PORT, () => {
+      console.log(`App is listingin on port ${PORT}`);
+    });
+  } catch (error) {
+    const resObj = {
+      message: "Server listning failed",
+      success: false,
+      error: error,
+    };
+    console.log(resObj);
+  }
 }
 
-main()
+main();
